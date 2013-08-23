1b15ae76c61970841501e18383dbc913e9a9dc21
Chris Pettitt <chris@samsarin.com>
2013-08-23 07:01:52 -0700

    Add "all" util function

--- test/util-test.js
+++ test/util-test.js
@@ -12,3 +12,26 @@ describe("util.sum", function() {
     assert.equal(util.sum([]), 0);
   });
 });
+
+describe("util.all", function() {
+  it("returns true if f(x) holds for all x in xs", function() {
+    assert.isTrue(util.all([1,2,3,4], function(x) {
+      return x > 0;
+    }));
+  });
+
+  it("returns false if f(x) does not hold for all x in xs", function() {
+    assert.isFalse(util.all([1,2,3,-1], function(x) {
+      return x > 0;
+    }));
+  });
+
+  it("fails fast if f(x) does not hold for all x in xs", function() {
+    var lastSeen;
+    assert.isFalse(util.all([1,2,-1,3,4], function(x) {
+      lastSeen = x;
+      return x > 0;
+    }));
+    assert.equal(lastSeen, -1);
+  });
+});
