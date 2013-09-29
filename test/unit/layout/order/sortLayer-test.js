dcbc5578a4f2c23b11203d51cdbeeb47377af08e
Chris Pettitt <chris@samsarin.com>
2013-09-28 20:13:42 -0700

    Initial work on compound graph crossing reduction in one layer

--- test/unit/layout/order/sortLayer-test.js
+++ test/unit/layout/order/sortLayer-test.js
@@ -48,4 +48,53 @@ describe('sortLayer', function() {
     assert.equal(g.node(2).order, 2);
     assert.equal(g.node(3).order, 1);
   });
+
+  it('handles a single subgraph', function() {
+    // We expect that nodes 1 and 2 will be adjacent since they are in the
+    // same subgraph, even though they'd otherwise be at opposite ends of the
+    // layer. The subgraph gets a weight of 1, which means it should come
+    // before node 3.
+
+    g.addNode(1, { rank: 0, order: 0 });
+    g.addNode(2, { rank: 0, order: 1 });
+    g.addNode(3, { rank: 0, order: 2 });
+    g.addNode('sg1', {});
+    g.parent(1, 'sg1');
+    g.parent(2, 'sg1');
+
+    var layerGraphs = initLayerGraphs(g);
+
+    sortLayer(layerGraphs[0],
+              { 1: [0, 0], 2: [2, 2], 3: [1, 2] });
+
+    assert.equal(g.node(1).order, 0);
+    assert.equal(g.node(2).order, 1);
+    assert.equal(g.node(3).order, 2);
+  });
+
+  it('handles nested subgraphs', function() {
+    g.addNode(1, { rank: 0 });
+    g.addNode(2, { rank: 0 });
+    g.addNode(3, { rank: 0 });
+    g.addNode(4, { rank: 0 });
+    g.addNode(5, { rank: 0 });
+    g.addNode('sg2', {});
+    g.parent(2, 'sg2');
+    g.parent(3, 'sg2');
+    g.addNode('sg1', {});
+    g.parent(1, 'sg1');
+    g.parent('sg2', 'sg1');
+    g.parent(4, 'sg1');
+
+    var layerGraphs = initLayerGraphs(g);
+
+    sortLayer(layerGraphs[0],
+              { 1: [4], 2: [3], 3: [7], 4: [6], 5: [6] });
+
+    assert.equal(g.node(1).order, 0);
+    assert.equal(g.node(2).order, 1);
+    assert.equal(g.node(3).order, 2);
+    assert.equal(g.node(4).order, 3);
+    assert.equal(g.node(5).order, 4);
+  });
 });
