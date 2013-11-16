cfa012039f730cc2e5fbf7774f51b958cf340942
Chris Pettitt <chris@samsarin.com>
2013-11-16 11:01:34 -0800

    More shuffling of tests

--- test/layout-test.js
+++ test/layout-test.js
@@ -1,45 +1,23 @@
 var assert = require('./assert'),
     layout = require('..').layout,
-    decode = require('graphlib').converter.json.decode,
-    Graph = require('graphlib').Graph;
     Digraph = require('graphlib').Digraph;
 
 describe('layout', function() {
-  it('preserves edge ids for graphs with edges spanning multiple ranks', function() {
-    var g = new Digraph();
-    g.addNode(1);
-    g.addNode(2);
-    g.addNode(3);
-    g.addEdge('1->2', 1, 2);
-    g.addEdge('2->3', 2, 3);
-    g.addEdge('1->3', 1, 3);
-    layout.apply(g);
-    assert.include(g.edges(), '1->3');
+  it('lays out out a graph with undefined node values', function() {
+    var inputGraph = new Digraph();
+    inputGraph.addNode(1);
+    var outputGraph = layout().run(inputGraph);
+    assert.property(outputGraph.node(1), 'x');
+    assert.property(outputGraph.node(1), 'y');
   });
 
-  it('performs simple layout for Graph without error', function() {
-    var nodes = [{id: 1, value: {width: 10, height: 10}},
-                 {id: 2, value: {width: 10, height: 10}}];
-    var edges = [{u: 1, v: 2}];
-
-    var g = layout()
-      .run(decode(nodes, edges, Graph));
-
-    // Simple check. node 1 and node 2 should not have the same y-coordiante.
-    var n1y = g.node(1).y;
-    var n2y = g.node(2).y;
-    assert.notEqual(n1y, n2y);
-  });
-
-  it('does not add dummy edges', function() {
+  it('lays out out a graph with undefined edge values', function() {
     var inputGraph = new Digraph();
     inputGraph.addNode(1);
     inputGraph.addNode(2);
-    inputGraph.addEdge('foo', 1, 2, { minLen: 4 });
-
+    inputGraph.addEdge('A', 1, 2);
     var outputGraph = layout().run(inputGraph);
-
-    assert.sameMembers(outputGraph.edges(), ['foo']);
+    assert.property(outputGraph.edge('A'), 'points');
   });
 
   it('includes bounding box information', function() {
@@ -52,5 +30,40 @@ describe('layout', function() {
 
     assert.equal(outputGraph.graph().bbox.width, 100);
     assert.equal(outputGraph.graph().bbox.height, 20 + 30 + layout().rankSep());
+  });
+
+  describe('rank constraints', function() {
+    var g;
+
+    beforeEach(function() {
+      g = new Digraph();
+      g.addNode(1, { width: 1, height: 1 });
+      g.addNode(2, { width: 1, height: 1, rank: 'same_1' });
+      g.addNode(3, { width: 1, height: 1, rank: 'same_1' });
+      g.addNode(4, { width: 1, height: 1 });
+      g.addNode(5, { width: 1, height: 1, rank: 'min' });
+      g.addNode(6, { width: 1, height: 1, rank: 'max' });
+
+      g.addEdge(null, 1, 2);
+      g.addEdge(null, 3, 4);
+      g.addEdge(null, 6, 1);
+    });
+    
+    it('ensures nodes with rank=min have the smallest y value', function() {
+      var out = layout().run(g);
+      var minY = Math.min.apply(Math, out.nodes().map(function(u) { return out.node(u).y; }));
+      assert.propertyVal(out.node(5), 'y', minY);
+    });
+
+    it('ensures nodes with rank=max have the greatest y value', function() {
+      var out = layout().run(g);
+      var maxY = Math.max.apply(Math, out.nodes().map(function(u) { return out.node(u).y; }));
+      assert.propertyVal(out.node(6), 'y', maxY);
+    });
+
+    it('ensures nodes with the rank=same_x have the same y value', function() {
+      var out = layout().run(g);
+      assert.equal(out.node(3).y, out.node(2).y);
+    });
   });
 });
