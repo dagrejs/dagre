3560e0affeb297d6767d55482580a0ba870bc526
Chris Pettitt <chris@samsarin.com>
2013-11-16 07:59:23 -0800

    Remove redundant layout tests
    The removed tests are covered by the smoke tests, where we can cover
    a number of different inputs.

--- test/layout-test.js
+++ test/layout-test.js
@@ -17,20 +17,6 @@ describe('layout', function() {
     assert.include(g.edges(), '1->3');
   });
 
-  it('preforms simple layout for Digraph without error', function() {
-    var nodes = [{id: 1, value: {width: 10, height: 10}},
-                 {id: 2, value: {width: 10, height: 10}}];
-    var edges = [{u: 1, v: 2}];
-
-    var g = layout()
-      .run(decode(nodes, edges));
-
-    // Simple check. node 1 should be above node 2.
-    var n1y = g.node(1).y;
-    var n2y = g.node(2).y;
-    assert.isTrue(n1y < n2y, 'node(1).y (' + n1y + ') should be above node(2).y (' + n2y + ')');
-  });
-
   it('performs simple layout for Graph without error', function() {
     var nodes = [{id: 1, value: {width: 10, height: 10}},
                  {id: 2, value: {width: 10, height: 10}}];
@@ -43,34 +29,6 @@ describe('layout', function() {
     var n1y = g.node(1).y;
     var n2y = g.node(2).y;
     assert.notEqual(n1y, n2y);
-  });
-
-  it('preserves edge ids', function() {
-    // This is a test that covers a bug we found where the original edge id was
-    // being lost when the final graph was constructed during layout.
-    var inputGraph = new Digraph();
-    inputGraph.addNode(1);
-    inputGraph.addNode(2);
-    inputGraph.addEdge('foo', 1, 2);
-
-    var outputGraph = layout().run(inputGraph);
-
-    assert.sameMembers(outputGraph.edges(), ['foo']);
-  });
-
-  it('preserves edge direction', function() {
-    var inputGraph = new Digraph();
-    inputGraph.addNode(1);
-    inputGraph.addNode(2);
-    inputGraph.addEdge('A', 1, 2, {});
-    inputGraph.addEdge('B', 2, 1, {});
-
-    var outputGraph = layout().run(inputGraph);
-
-    assert.equal(outputGraph.source('A'), 1);
-    assert.equal(outputGraph.target('A'), 2);
-    assert.equal(outputGraph.source('B'), 2);
-    assert.equal(outputGraph.target('B'), 1);
   });
 
   it('does not add dummy edges', function() {
