44655f11e141fc15514c210e0b48a1b3662caefc
Chris Pettitt <cpettitt@gmail.com>
2014-08-26 06:31:29 -0700

    Update license years

--- browser.js
+++ browser.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (c) 2012-2013 Chris Pettitt
+ * Copyright (c) 2012-2014 Chris Pettitt
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
