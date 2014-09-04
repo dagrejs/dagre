31567eadf016ca8f38ba972c25c20f8de4dbcc27
Chris Pettitt <cpettitt@gmail.com>
2014-09-03 17:27:32 -0700

    Remove edgeIter

--- test/rank/leave-edge-test.js
+++ test/rank/leave-edge-test.js
@@ -1,6 +1,5 @@
 var expect = require("../chai").expect,
     Graph = require("graphlib").Graph,
-    edgeIter = require("../../lib/rank/edge-iter"),
     leaveEdge = require("../../lib/rank/leave-edge");
 
 describe("rank/leaveEdge", function() {
@@ -8,13 +7,13 @@ describe("rank/leaveEdge", function() {
     var tree = new Graph();
     tree.setEdge("a", "b", { cutvalue: 1 });
     tree.setEdge("b", "c", { cutvalue: 1 });
-    expect(leaveEdge(edgeIter(tree))).to.be.undefined;
+    expect(leaveEdge(tree)).to.be.undefined;
   });
 
   it("returns an edge if one is found with a negative cutvalue", function() {
     var tree = new Graph();
     tree.setEdge("a", "b", { cutvalue: 1 });
     tree.setEdge("b", "c", { cutvalue: -1 });
-    expect(leaveEdge(edgeIter(tree))).to.eql({ v: "b", w: "c", label: { cutvalue: -1 }});
+    expect(leaveEdge(tree)).to.eql({ v: "b", w: "c", label: { cutvalue: -1 }});
   });
 });
