5f9c46c11e315e975b49fe04607987c611e5489c
Chris Pettitt <cpettitt@gmail.com>
2014-09-07 17:43:55 -0700

    More testing for layout

--- test/layout-test.js
+++ test/layout-test.js
@@ -1,4 +1,5 @@
-var expect = require("./chai").expect,
+var _ = require("lodash"),
+    expect = require("./chai").expect,
     layout = require("..").layout,
     Digraph = require("graphlib").Digraph;
 
@@ -12,7 +13,27 @@ describe("layout", function() {
   it("can layout a single node", function() {
     g.setNode("a", { width: 50, height: 100 });
     layout(g);
+    expect(extractCoordinates(g)).to.eql({
+      a: { x: 50 / 2, y: 100 / 2 }
+    });
     expect(g.getNode("a").x).to.equal(50 / 2);
     expect(g.getNode("a").y).to.equal(100 / 2);
   });
+
+  it("can layout two nodes on the same rank", function() {
+    g.getGraph().nodesep = 200;
+    g.setNode("a", { width: 50, height: 100 });
+    g.setNode("b", { width: 75, height: 200 });
+    layout(g);
+    expect(extractCoordinates(g)).to.eql({
+      a: { x: 50 / 2,            y: 200 / 2 },
+      b: { x: 50 + 200 + 75 / 2, y: 200 / 2 }
+    });
+  });
 });
+
+function extractCoordinates(g) {
+  var nodes = g.nodes();
+  return _.zipObject(_.pluck(nodes, "v"),
+                     _.map(nodes, function(node) { return _.pick(node.label, ["x", "y"]); }));
+}
