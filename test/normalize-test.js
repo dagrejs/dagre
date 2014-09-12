07a09547ac5c0eb99b82cf3a103f2bdbe2b8e62f
Chris Pettitt <cpettitt@gmail.com>
2014-09-12 08:56:52 -0700

    Add width / height to dummy nodes

--- test/normalize-test.js
+++ test/normalize-test.js
@@ -39,6 +39,19 @@ describe("normalize", function() {
       expect(g.getNode("a").rank).to.equal(0);
       expect(g.getNode("b").rank).to.equal(2);
     });
+
+    it("assigns width = 0, height = 0 to dummy nodes for an edge with no label", function() {
+      g.setNode("a", { rank: 0 });
+      g.setNode("b", { rank: 2 });
+      g.setEdge("a", "b");
+
+      normalize.run(g);
+
+      expect(g.successors("a")).to.have.length(1);
+      var successor = g.successors("a")[0];
+      expect(g.getNode(successor).width).to.equal(0);
+      expect(g.getNode(successor).height).to.equal(0);
+    });
   });
 
   describe("undo", function() {
