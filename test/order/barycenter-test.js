6b5cb82eb7023eec7032c52f4f70e8b55e2a70f5
Chris Pettitt <cpettitt@gmail.com>
2014-09-13 16:39:32 -0700

    Initial work on barycenter sort

---
+++ test/order/barycenter-test.js
@@ -0,0 +1,36 @@
+var expect = require("../chai").expect,
+    barycenter = require("../../lib/order/barycenter");
+
+describe("order/barycenter", function() {
+  it("sorts based on barycenters", function() {
+    var fixed = ["b1", "b2", "b3"],
+        movable = ["a3", "a2", "a1"],
+        neighbors = { a1: { b1: 1 }, a2: { b2: 1 }, a3: { b3: 1 } };
+    barycenter(fixed, movable, neighbors);
+    expect(movable).eqls(["a1", "a2", "a3"]);
+  });
+
+  it("sorts around nodes with no neighbors", function() {
+    var fixed = ["b1", "b2", "b3"],
+        movable = ["a4", "a3", "a2", "a1"],
+        neighbors = { a1: { b1: 1 }, a2: { b2: 1 }, a4: { b3: 1 } };
+    barycenter(fixed, movable, neighbors);
+    expect(movable).eqls(["a1", "a3", "a2", "a4"]);
+  });
+
+  it("handles multiple edges", function() {
+    var fixed = ["b1", "b2"],
+        movable = ["a3", "a2", "a1"],
+        neighbors = { a1: { b1: 1, b2: 1 }, a2: { b1: 1 }, a3: { b2: 1 } };
+    barycenter(fixed, movable, neighbors);
+    expect(movable).eqls(["a2", "a1", "a3"]);
+  });
+
+  it("accounts for weights", function() {
+    var fixed = ["b1", "b2"],
+        movable = ["a2", "a3", "a1"],
+        neighbors = { a1: { b1: 2, b2: 1 }, a2: { b1: 1, b2: 1 }, a3: { b1: 1, b2: 2 } };
+    barycenter(fixed, movable, neighbors);
+    expect(movable).eqls(["a1", "a2", "a3"]);
+  });
+});
