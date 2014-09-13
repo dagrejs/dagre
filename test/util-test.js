78930237a1b5d995b6f77041c1a4c647798a4a8f
Chris Pettitt <cpettitt@gmail.com>
2014-09-12 17:24:36 -0700

    Add rectangle intersection code

--- test/util-test.js
+++ test/util-test.js
@@ -4,6 +4,48 @@ var _ = require("lodash"),
     util = require("../lib/util");
 
 describe("util", function() {
+  describe("intersectRect", function() {
+    function expectIntersects(rect, point) {
+      var cross = util.intersectRect(rect, point);
+      if (cross.x !== point.x) {
+        var m = (cross.y - point.y) / (cross.x - point.x);
+        expect(cross.y - rect.y).equals(m * (cross.x - rect.x));
+      }
+    }
+
+    function expectTouchesBorder(rect, point) {
+      var cross = util.intersectRect(rect, point);
+      if (Math.abs(rect.x - cross.x) !== rect.width / 2) {
+        expect(Math.abs(rect.y - cross.y)).equals(rect.height / 2);
+      }
+    }
+
+    it("creates a slope that will intersect the rectangle's center", function() {
+      var rect = { x: 0, y: 0, width: 1, height: 1 };
+      expectIntersects(rect, { x:  2, y:  6 });
+      expectIntersects(rect, { x:  2, y: -6 });
+      expectIntersects(rect, { x:  6, y:  2 });
+      expectIntersects(rect, { x: -6, y:  2 });
+      expectIntersects(rect, { x:  5, y:  0 });
+      expectIntersects(rect, { x:  0, y:  5 });
+    });
+
+    it("touches the border of the rectangle", function() {
+      var rect = { x: 0, y: 0, width: 1, height: 1 };
+      expectTouchesBorder(rect, { x:  2, y:  6 });
+      expectTouchesBorder(rect, { x:  2, y: -6 });
+      expectTouchesBorder(rect, { x:  6, y:  2 });
+      expectTouchesBorder(rect, { x: -6, y:  2 });
+      expectTouchesBorder(rect, { x:  5, y:  0 });
+      expectTouchesBorder(rect, { x:  0, y:  5 });
+    });
+
+    it("throws an error if the point is at the center of the rectangle", function() {
+      var rect = { x: 0, y: 0, width: 1, height: 1 };
+      expect(function() { util.intersectRect(rect, { x: 0, y: 0 }); }).to.throw();
+    });
+  });
+
   describe("buildLayerMatrix", function() {
     it("creates a matrix based on rank and order of nodes in the graph", function() {
       var g = new Graph();
