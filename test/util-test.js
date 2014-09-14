a0d634e459d50e47aca55576bb2027e2859fb512
Chris Pettitt <cpettitt@gmail.com>
2014-09-13 18:11:28 -0700

    Initial hook up for barycenter into order phase

--- test/util-test.js
+++ test/util-test.js
@@ -28,31 +28,31 @@ describe("util", function() {
     });
   });
 
-  describe("weightedSuccessors", function() {
+  describe("successorWeights", function() {
     it("maps a node to its successors with associated weights", function() {
        var g = new Graph({ multigraph: true });
        g.setEdge("a", "b", { weight: 2 });
        g.setEdge("b", "c", { weight: 1 });
        g.setEdge("b", "c", { weight: 2 }, "multi");
        g.setEdge("b", "d", { weight: 1 }, "multi");
-       expect(util.weightedSuccessors(g).a).to.eql({ b: 2 });
-       expect(util.weightedSuccessors(g).b).to.eql({ c: 3, d: 1 });
-       expect(util.weightedSuccessors(g).c).to.eql({});
-       expect(util.weightedSuccessors(g).d).to.eql({});
+       expect(util.successorWeights(g).a).to.eql({ b: 2 });
+       expect(util.successorWeights(g).b).to.eql({ c: 3, d: 1 });
+       expect(util.successorWeights(g).c).to.eql({});
+       expect(util.successorWeights(g).d).to.eql({});
     });
   });
 
-  describe("weightedPredecessors", function() {
+  describe("predecessorWeights", function() {
     it("maps a node to its predecessors with associated weights", function() {
        var g = new Graph({ multigraph: true });
        g.setEdge("a", "b", { weight: 2 });
        g.setEdge("b", "c", { weight: 1 });
        g.setEdge("b", "c", { weight: 2 }, "multi");
        g.setEdge("b", "d", { weight: 1 }, "multi");
-       expect(util.weightedPredecessors(g).a).to.eql({});
-       expect(util.weightedPredecessors(g).b).to.eql({ a: 2 });
-       expect(util.weightedPredecessors(g).c).to.eql({ b: 3 });
-       expect(util.weightedPredecessors(g).d).to.eql({ b: 1 });
+       expect(util.predecessorWeights(g).a).to.eql({});
+       expect(util.predecessorWeights(g).b).to.eql({ a: 2 });
+       expect(util.predecessorWeights(g).c).to.eql({ b: 3 });
+       expect(util.predecessorWeights(g).d).to.eql({ b: 1 });
     });
   });
 
