87d9bbcb9bb188fe4b740d4469cc6a0d4326ed31
Chris Pettitt <cpettitt@gmail.com>
2014-09-18 18:56:24 -0700

    Sort by subgraph across layers
    Still need to prevent subgraphs from getting intertwined across layers.

--- test/order/barycenter-test.js
+++ test/order/barycenter-test.js
@@ -1,51 +1,65 @@
 var expect = require("../chai").expect,
-    barycenter = require("../../lib/order/barycenter");
+    barycenter = require("../../lib/order/barycenter"),
+    Graph = require("graphlib").Graph;
 
 describe("order/barycenter", function() {
-  it("does not assign a value to nodes with no adjacencies", function() {
-    var fixed = [],
-        movable = ["a"],
-        neighbors = {};
+  var g;
 
-    var results = barycenter(fixed, movable, neighbors);
-    expect(results).to.not.have.property("a");
+  beforeEach(function() {
+    g = new Graph()
+      .setDefaultNodeLabel(function() { return {}; })
+      .setDefaultEdgeLabel(function() { return { weight: 1 }; });
   });
 
-  it("assigns the position of the sole adjacency", function() {
-    var fixed = ["a", "b", "c"],
-        movable = ["x"],
-        neighbors = { x: { c: 1 } };
+  it("assigns an undefined barycenter for a node with no predecessors", function() {
+    g.setNode("x", {});
 
-    var results = barycenter(fixed, movable, neighbors);
-    expect(results.x).to.eql({ barycenter: 2, weight: 1 });
+    barycenter(g, ["x"]);
+    expect(g.getNode("x").barycenter).to.be.undefined;
+    expect(g.getNode("x").barycenterWeight).to.be.undefined;
   });
 
-  it("assigns the average of multiple adjacencies", function() {
-    var fixed = ["a", "b", "c", "d", "e"],
-        movable = ["x"],
-        neighbors = { x: { c: 1, e: 1 } };
+  it("assigns the position of the sole predecessors", function() {
+    g.setNode("a", { order: 2 });
+    g.setEdge("a", "x");
 
-    var results = barycenter(fixed, movable, neighbors);
-    expect(results.x).to.eql({ barycenter: 3, weight: 2 });
+    barycenter(g, ["x"]);
+    expect(g.getNode("x")).eqls({ barycenter: 2, barycenterWeight: 1 });
+  });
+
+  it("assigns the average of multiple predecessors", function() {
+    g.setNode("a", { order: 2 });
+    g.setNode("b", { order: 4 });
+    g.setEdge("a", "x");
+    g.setEdge("b", "x");
+
+    barycenter(g, ["x"]);
+    expect(g.getNode("x")).eqls({ barycenter: 3, barycenterWeight: 2 });
   });
 
   it("takes into account the weight of edges", function() {
-    var fixed = ["a", "b", "c", "d", "e"],
-        movable = ["x"],
-        neighbors = { x: { c: 3, e: 1 } };
+    g.setNode("a", { order: 2 });
+    g.setNode("b", { order: 4 });
+    g.setEdge("a", "x", { weight: 3 });
+    g.setEdge("b", "x");
 
-    var results = barycenter(fixed, movable, neighbors);
-    expect(results.x).to.eql({ barycenter: 2.5, weight: 4 });
+    barycenter(g, ["x"]);
+    expect(g.getNode("x")).eqls({ barycenter: 2.5, barycenterWeight: 4 });
   });
 
   it("calculates barycenters for all nodes in the movable layer", function() {
-    var fixed = ["a", "b", "c", "d", "e"],
-        movable = ["x", "y", "z"],
-        neighbors = { x: { b: 1, c: 1 }, z: { b: 2, e: 1 } };
+    g.setNode("a", { order: 1 });
+    g.setNode("b", { order: 2 });
+    g.setNode("c", { order: 4 });
+    g.setEdge("a", "x");
+    g.setEdge("b", "x");
+    g.setNode("y");
+    g.setEdge("a", "z", { weight: 2 });
+    g.setEdge("c", "z");
 
-    var results = barycenter(fixed, movable, neighbors);
-    expect(results.x).to.eql({ barycenter: 1.5, weight: 2 });
-    expect(results.y).to.be.undefined;
-    expect(results.z).to.eql({ barycenter: 2, weight: 3 });
+    barycenter(g, ["x", "y", "z"]);
+    expect(g.getNode("x")).eqls({ barycenter: 1.5, barycenterWeight: 2 });
+    expect(g.getNode("y").barycenter).to.be.undefined;
+    expect(g.getNode("z")).eqls({ barycenter: 2, barycenterWeight: 3 });
   });
 });
---
+++ test/order/sort-subgraph-test.js
@@ -0,0 +1,103 @@
+var _ = require("lodash"),
+    expect = require("../chai").expect,
+    sortSubgraph = require("../../lib/order/sort-subgraph"),
+    Graph = require("graphlib").Graph;
+
+describe("order/sortSubgraph", function() {
+  var g;
+
+  beforeEach(function() {
+    g = new Graph({ compound: true })
+      .setDefaultNodeLabel(function() { return {}; })
+      .setDefaultEdgeLabel(function() { return { weight: 1 }; });
+    _.each(_.range(5), function(v) { g.setNode(v, { order: v }); });
+  });
+
+  it("sorts a flat subgraph based on barycenter", function() {
+    g.setEdge(3, "x");
+    g.setEdge(1, "y", { weight: 2 });
+    g.setEdge(4, "y");
+    _.each(["x", "y"], function(v) { g.setParent(v, "movable"); });
+
+    expect(sortSubgraph(g, "movable").list).eqls(["y", "x"]);
+  });
+
+  it("preserves the pos of a node (y) w/o neighbors in a flat subgraph", function() {
+    g.setEdge(3, "x");
+    g.setNode("y");
+    g.setEdge(1, "z", { weight: 2 });
+    g.setEdge(4, "z");
+    _.each(["x", "y", "z"], function(v) { g.setParent(v, "movable"); });
+
+    expect(sortSubgraph(g, "movable").list).eqls(["z", "y", "x"]);
+  });
+
+  it("biases to the left without reverse bias", function() {
+    g.setEdge(1, "x");
+    g.setEdge(1, "y");
+    _.each(["x", "y"], function(v) { g.setParent(v, "movable"); });
+
+    expect(sortSubgraph(g, "movable").list).eqls(["x", "y"]);
+  });
+
+  it("biases to the right with reverse bias", function() {
+    g.setEdge(1, "x");
+    g.setEdge(1, "y");
+    _.each(["x", "y"], function(v) { g.setParent(v, "movable"); });
+
+    expect(sortSubgraph(g, "movable", true).list).eqls(["y", "x"]);
+  });
+
+  it("aggregates stats about the subgraph", function() {
+    g.setEdge(3, "x");
+    g.setEdge(1, "y", { weight: 2 });
+    g.setEdge(4, "y");
+    _.each(["x", "y"], function(v) { g.setParent(v, "movable"); });
+
+    var results = sortSubgraph(g, "movable");
+    expect(results.barycenter).to.equal(2.25);
+    expect(results.barycenterWeight).to.equal(4);
+  });
+
+  it("can sort a nested subgraph with no barycenter", function() {
+    g.setNodes(["a", "b", "c"]);
+    g.setParent("a", "y");
+    g.setParent("b", "y");
+    g.setParent("c", "y");
+    g.setEdge(0, "x");
+    g.setEdge(1, "z");
+    g.setEdge(2, "y");
+    _.each(["x", "y", "z"], function(v) { g.setParent(v, "movable"); });
+
+    var list = sortSubgraph(g, "movable").list;
+    expect(list).eqls(["x", "z", "a", "b", "c"]);
+  });
+
+  it("can sort a nested subgraph with a barycenter", function() {
+    g.setNodes(["a", "b", "c"]);
+    g.setParent("a", "y");
+    g.setParent("b", "y");
+    g.setParent("c", "y");
+    g.setEdge(0, "a", { weight: 3 });
+    g.setEdge(0, "x");
+    g.setEdge(1, "z");
+    g.setEdge(2, "y");
+    _.each(["x", "y", "z"], function(v) { g.setParent(v, "movable"); });
+
+    expect(sortSubgraph(g, "movable").list).eqls(["x", "a", "b", "c", "z"]);
+  });
+
+  it("can sort a nested subgraph with no in-edges", function() {
+    g.setNodes(["a", "b", "c"]);
+    g.setParent("a", "y");
+    g.setParent("b", "y");
+    g.setParent("c", "y");
+    g.setEdge(0, "a");
+    g.setEdge(1, "b");
+    g.setEdge(0, "x");
+    g.setEdge(1, "z");
+    _.each(["x", "y", "z"], function(v) { g.setParent(v, "movable"); });
+
+    expect(sortSubgraph(g, "movable").list).eqls(["x", "a", "b", "c", "z"]);
+  });
+});
