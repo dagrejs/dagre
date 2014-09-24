e02c4ea18a70a19608df95627e7a616849671155
Chris Pettitt <cpettitt@gmail.com>
2014-09-24 06:20:08 -0700

    Use border nodes when assigning barycenters

--- test/order/sort-subgraph-test.js
+++ test/order/sort-subgraph-test.js
@@ -109,4 +109,18 @@ describe("order/sortSubgraph", function() {
     _.each(["x", "y", "z", "bl", "br"], function(v) { g.setParent(v, "sg1"); });
     expect(sortSubgraph(g, "sg1", cg).vs).eqls(["bl", "x", "y", "z", "br"]);
   });
+
+  it("assigns a barycenter to a subgraph based on previous border nodes", function() {
+    g.setNode("bl1", { order: 0 });
+    g.setNode("br1", { order: 1 });
+    g.setEdge("bl1", "bl2");
+    g.setEdge("br1", "br2");
+    _.each(["bl2", "br2"], function(v) { g.setParent(v, "sg"); });
+    g.setNode("sg", { borderLeft: "bl2", borderRight: "br2" });
+    expect(sortSubgraph(g, "sg", cg)).eqls({
+      barycenter: 0.5,
+      weight: 2,
+      vs: ["bl2", "br2"]
+    });
+  });
 });
