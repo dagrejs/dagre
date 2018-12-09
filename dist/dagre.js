99a59a9632f48147f7ea674a4a127b7d0346f7b8
Lutz Roeder <lutzroeder@users.noreply.github.com>
2018-12-08 16:37:37 -0800

    Prep v0.8.3 release

--- dist/dagre.js
+++ dist/dagre.js
@@ -2921,7 +2921,7 @@ function notime(name, fn) {
 }
 
 },{"./graphlib":7,"./lodash":10}],30:[function(require,module,exports){
-module.exports = "0.8.3-pre";
+module.exports = "0.8.3";
 
 },{}],31:[function(require,module,exports){
 /**
