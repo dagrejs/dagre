d48c680f74ef12d40808a101347ccd80b75ae860
Anya <anyazhang01@yahoo.com>
2019-07-01 12:11:10 +0200

    Added nodes and single layer test to cross count tests

--- test/order/cross-count-test.js
+++ test/order/cross-count-test.js
@@ -1,6 +1,8 @@
 var expect = require("../chai").expect;
 var Graph = require("../../lib/graphlib").Graph;
 var crossCount = require("../../lib/order/cross-count");
+var _ = require("lodash");
+
 
 describe("crossCount", function() {
   var g;
@@ -8,6 +10,9 @@ describe("crossCount", function() {
   beforeEach(function() {
     g = new Graph()
       .setDefaultEdgeLabel(function() { return { weight: 1 }; });
+      _.forEach(["a1", "a2", "a3"], function(v) { g.setNode(v, { rank: 1 }); });
+      _.forEach(["b1", "b2"], function(v) { g.setNode(v, { rank: 2 }); });
+      _.forEach(["c1", "c2"], function(v) { g.setNode(v, { rank: 3 }); });
   });
 
   it("returns 0 for an empty layering", function() {
@@ -39,6 +44,9 @@ describe("crossCount", function() {
   });
 
   it("works for graph #1", function() {
+    _.forEach(["a", "d"], function(v) { g.setNode(v, { rank: 1 }); });
+    _.forEach(["b", "f", "e"], function(v) { g.setNode(v, { rank: 2 }); });
+    _.forEach(["c", "i"], function(v) { g.setNode(v, { rank: 3 }); });
     g.setPath(["a", "b", "c"]);
     g.setPath(["d", "e", "c"]);
     g.setPath(["a", "f", "i"]);
@@ -46,4 +54,12 @@ describe("crossCount", function() {
     expect(crossCount(g, [["a", "d"], ["b", "e", "f"], ["c", "i"]])).equals(1);
     expect(crossCount(g, [["d", "a"], ["e", "b", "f"], ["c", "i"]])).equals(0);
   });
+
+  it("handles single-layer crossings", function() {
+    g.setEdge("a1", "b1");
+    g.setEdge("a1", "a3");
+    g.setEdge("a2", "b2");
+    expect(crossCount(g, [["a1", "a2", "a3"], ["b2", "b1"]])).equals(2);
+  });
+
 });
