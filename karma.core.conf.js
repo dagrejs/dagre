7b438a53a386d9c92ea8d5a4bd888a243d460b7d
Tim Paine <t.paine154@gmail.com>
2022-05-05 22:06:14 -0400

    move to github actions
    tweak readme for build status, will change before merging
    
    tweak for display
    
    Try without
    
    try this
    
    bump deps, use yarn
    
    remap badge
    
    Revert "bump deps, use yarn"
    
    This reverts commit de605aaad49400d75459fdf1e2586c20c472ffba.
    
    drop phantomjs

--- karma.core.conf.js
+++ karma.core.conf.js
@@ -60,7 +60,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox', 'PhantomJS'],
+    browsers: ['Chrome', 'Firefox'],
 
 
     // Continuous Integration mode
