c1c33093de955346c6b6ce8b2ce32f2d2e77f756
MatsSchrader <mats.schrader@uni-hamburg.de>
2025-04-07 16:20:14 +0200

    experimenting with previous ranks as initial ranks

--- local-execution/main.js
+++ local-execution/main.js
@@ -1,31 +1,77 @@
 import * as dagre from 'dagre';
 import * as d3 from 'd3';
+import * as v from "./graphVariants.js";
 
 // Create a new directed graph
 const g = new dagre.graphlib.Graph();
 
+//TODO previous ranks
+export function initRanksDynamic(g) {
+  debugger;
+  g.nodes().forEach(v => {
+    g.node(v).rank = prevNodeRankMap[v] || 20; // Default to 0 if not found
+  });
+}
+
+const prevNodeRankMap = {
+  "VK-Lieferung": 8,
+  "Auftragserstellung": 4,
+  "VK-Rechnung": 10,
+  "Start": 0,
+  "Zahlungseingang": 12,
+  "End": 14,
+  "zugehöriger Wareneingang": 6,
+  "Anlage der Bestellung": 2,
+  "Reisekostenerfassung": 10,
+  "Durchführung der Reise": 8,
+  "Reisekostenfreigabe 2": 6,
+  "Reisekostenantrag": 2,
+  "Reisekostenfreigabe": 4,
+  "Verbuchung/Kontierung": 12
+};
+
 // Set an object for the graph label
-g.setGraph({});
+g.setGraph({
+  rankdir: 'LR',
+  nodesep: 50,
+  edgesep: 10,
+  ranksep: 75,
+});
 
 // Default to assigning a new object as a label for each new edge.
 g.setDefaultEdgeLabel(() => ({}));
 
-// Add nodes to the graph. The first argument is the node id. The second is
-// metadata about the node. In this case we're going to add labels to each of
-// our nodes.
-g.setNode("kspacey",    { label: "Kevin Spacey",  width: 144, height: 100 });
-g.setNode("swilliams",  { label: "Saul Williams", width: 160, height: 100 });
-g.setNode("bpitt",      { label: "Brad Pitt",     width: 108, height: 100 });
-g.setNode("hford",      { label: "Harrison Ford", width: 168, height: 100 });
-g.setNode("lwilson",    { label: "Luke Wilson",   width: 144, height: 100 });
-g.setNode("kbacon",     { label: "Kevin Bacon",   width: 121, height: 100 });
+//TODO change variant to what I want to test
+const variant = v.variant1to4;
 
-// Add edges to the graph.
-g.setEdge("kspacey",   "swilliams");
-g.setEdge("swilliams", "kbacon");
-g.setEdge("bpitt",     "kbacon");
-g.setEdge("hford",     "lwilson");
-g.setEdge("lwilson",   "kbacon");
+//TODO layout one graph, save ranks (g.nodes[i].rank) and then layout the other graph with these as initial ranks/final ranks
+// same with order?
+
+//TODO adjust weights:
+// - according to targetProcess (similarity)
+// - according to previous layout
+
+//TODO implement slider as in process analyzer
+// save variants in a way to access them according to slider
+// save object per variant add all variants per slider and ignore duplicates?
+
+// Add nodes to the graph
+variant.nodes.forEach(node => {
+  g.setNode(node.id, {
+    label: node.label,
+    width: node.width,
+    height: node.height
+  });
+});
+
+// Add edges to the graph
+variant.edges.forEach((edge, i) => {
+  g.setEdge(edge.source, edge.target, {
+    weight: edge.weight || 1,
+    minlen: 1, // Default minimum length
+    id: `e${i}`  // Generate a unique ID for each edge
+  });
+});
 
 // Add a debugger statement to pause execution before layout calculation
 console.log("About to run dagre layout algorithm");
