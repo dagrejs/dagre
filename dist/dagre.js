a7c0bd1c0edf89c53c3efd05557c9920289d2fd2
David Newell <beta@alumni.rice.edu>
2026-02-09 20:58:05 +0100

    Bumping version for release

--- dist/dagre.js
+++ dist/dagre.js
@@ -2279,7 +2279,7 @@ var dagre = (() => {
   // lib/version.js
   var require_version = __commonJS({
     "lib/version.js"(exports, module) {
-      module.exports = "2.0.4-pre";
+      module.exports = "2.0.4";
     }
   });
 
