b2ab223e088610016d88daf0ccbaee7f36ea5ffd
Wandrille <wandrille.guesdon@gmail.com>
2026-02-28 13:17:31 +0100

    feat: add missing constraints to layout configuration in new TS file

--- lib/types.ts
+++ lib/types.ts
@@ -108,6 +108,7 @@ export interface EdgeConfig {
 export interface LayoutConfig {
     customOrder?: (graph: Graph<GraphLabel, NodeLabel, EdgeLabel>, order: (graph: Graph<GraphLabel, NodeLabel, EdgeLabel>, opts: LayoutOptions) => void) => void;
     disableOptimalOrderHeuristic?: boolean;
+    constraints?: Array<{ left: string; right: string }>
 }
 
 // Order constraint for ordering heuristic
